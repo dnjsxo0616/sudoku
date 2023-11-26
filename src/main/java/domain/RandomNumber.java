6471585894dec40718b2d61f792057a7b5b3f79c
KDT live <dnjsxo0616@gmail.com>
2023-11-26 15:55:42 +0900

    refactor: 랜덤 숫자 메서드 수정

---
+++ src/main/java/domain/RandomNumber.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomNumber {
+
+    private final List<Integer> randomNumber;
+
+    public RandomNumber() {
+        this.randomNumber = createNumber();
+    }
+
+    ;
+
+    public List<Integer> createNumber() {
+        List<Integer> numbers = IntStream.rangeClosed(1, 8).boxed().collect(Collectors.toList());
+
+        Collections.shuffle(numbers);
+
+        return numbers;
+    }
+
+    public List<Integer> getRandomNumber() {
+        return randomNumber;
+    }
+
+}
