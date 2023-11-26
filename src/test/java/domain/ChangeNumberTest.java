a8ac0fe4b39e3964ebd79addc11393a75da81e1f
KDT live <dnjsxo0616@gmail.com>
2023-11-26 17:55:02 +0900

    test: 중복 검증 테스트

--- src/test/java/domain/ChangeNumberTest.java
+++ src/test/java/domain/ChangeNumberTest.java
@@ -17,4 +17,11 @@ class ChangeNumberTest {
     void ChangeNumber_생성_오류_테스트() {
         assertThrows(IllegalArgumentException.class, () -> new ChangeNumber(" 2,3"));
     }
+
+    @Test
+    void ChangeNumber_중복_오류_테스트() {
+        Assertions.assertThatThrownBy( () -> new ChangeNumber("2, 2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 숫자가 존재합니다.");
+    }
 }
