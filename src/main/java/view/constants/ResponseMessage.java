19b0574523717cba36151e919d918d684d293e01
KDT live <dnjsxo0616@gmail.com>
2023-11-26 23:25:41 +0900

    feat: Turn 카운트 출력 기능

--- src/main/java/view/constants/ResponseMessage.java
+++ src/main/java/view/constants/ResponseMessage.java
@@ -1,10 +1,13 @@
 package view.constants;
 
+import domain.CountRound;
+
 public enum ResponseMessage {
 
     RESPONSE_MAIN_START("간단 숫자 퍼즐"),
     RESPONSE_CHANGE_NUMBER("교환할 두 숫자를 입력>"),
-    RESPONSE_COMPLETE("축하합니다! 3턴만에 퍼즐을 완성하셨습니다!");
+    RESPONSE_COMPLETE("축하합니다! 3턴만에 퍼즐을 완성하셨습니다!"),
+    RESPONSE_TURN_COUNT("Turn %d");
     private final String message;
 
     ResponseMessage(String message) {
@@ -13,6 +16,10 @@ public enum ResponseMessage {
 
     public String getMessage() {
         return message;
+    }
+
+    public String getFormattedMessage(int count) {
+        return String.format(message, count);
     }
 }
 
