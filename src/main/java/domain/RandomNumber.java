48c13e30386951c59ca0451e8bd563e83748725e
KDT live <dnjsxo0616@gmail.com>
2023-11-26 18:41:16 +0900

    feat: 숫자 변경 기능과 정답 확인 기능 추가

--- src/main/java/domain/RandomNumber.java
+++ src/main/java/domain/RandomNumber.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,6 +26,22 @@ public class RandomNumber {
 
     public List<Integer> getRandomNumber() {
         return Collections.unmodifiableList(randomNumber);
+    }
+
+    public void changePosition(ChangeNumber changeNumber) {
+        List<Integer> changeNumbers = changeNumber.getChangeNumber();
+
+        int firstIndex = randomNumber.indexOf(changeNumbers.get(0));
+        int secondIndex = randomNumber.indexOf(changeNumbers.get(1));
+
+        Integer temp = randomNumber.get(firstIndex);
+        randomNumber.set(firstIndex, randomNumber.get(secondIndex));
+        randomNumber.set(secondIndex, temp);
+    }
+
+    public boolean checkNumber(){
+        List<Integer> answerNumber = Arrays.asList(1,2,3,4,5,6,7,8);
+        return randomNumber.equals(answerNumber);
     }
 
 }
