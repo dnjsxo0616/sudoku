92472d75bc825bb4b5378b15224394eefa4c724b
KDT live <dnjsxo0616@gmail.com>
2023-11-26 23:19:47 +0900

    feat: Turn 카운트 기능

---
+++ src/main/java/domain/CountRound.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class CountRound {
+    private int count = 0;
+
+    public void increaseCount() {
+        this.count++;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+}
