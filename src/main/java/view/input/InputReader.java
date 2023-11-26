adc94b880adf2d289615b7545c51652a07a974e6
KDT live <dnjsxo0616@gmail.com>
2023-11-26 16:54:43 +0900

    feat: 입력 기능 추가

---
+++ src/main/java/view/input/InputReader.java
@@ -0,0 +1,10 @@
+package view.input;
+
+import java.util.Scanner;
+
+public class InputReader {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static String readInput() {
+        return scanner.nextLine();
+    }
+}
