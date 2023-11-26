e167a2f542dc7ca8958ab053a7d19a6fc8bc1646
KDT live <dnjsxo0616@gmail.com>
2023-11-26 23:08:22 +0900

    feat: 완성 메시지 추가

--- src/main/java/view/constants/ResponseMessage.java
+++ src/main/java/view/constants/ResponseMessage.java
@@ -3,7 +3,8 @@ package view.constants;
 public enum ResponseMessage {
 
     RESPONSE_MAIN_START("간단 숫자 퍼즐"),
-    RESPONSE_CHANGE_NUMBER("교환할 두 숫자를 입력>");
+    RESPONSE_CHANGE_NUMBER("교환할 두 숫자를 입력>"),
+    RESPONSE_COMPLETE("축하합니다! 3턴만에 퍼즐을 완성하셨습니다!");
     private final String message;
 
     ResponseMessage(String message) {
